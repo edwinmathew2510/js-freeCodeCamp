54fd6baab999b5ea9163fd2e6c3e407e30b91f7b
Edwin Mathew <edwintm2510@gmail.com>
2025-05-02 16:01:35 +0530

    string concat

---
+++ index.js
@@ -0,0 +1,12 @@
+//string concat
+let name = "edwin";
+name += " mathew";
+console.log(name);
+
+let str1 = "hello";
+let str2 = "world";
+let result = str1.concat(" ", str2);
+console.log(typeof result); // Output: "hello world"
+
+let str = 'it is a "beautiful day!"';
+console.log(str); // Output: hello "world"
