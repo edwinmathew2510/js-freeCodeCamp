c31abbfa91e3c04576580c9ce44687b1648ac465
Edwin Mathew <edwintm2510@gmail.com>
2025-05-04 23:01:06 +0530

    ASCII VALUE

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-//positioning substrings by index of
+//ASCII VALUE
+let num = "5"; //char code at
+console.log(num.charCodeAt(0));
 
-let str = "hi Edwin";
-let position = str.indexOf("Edwin");
-console.log(position);
+let num1 = String.fromCharCode(67);
+console.log(num); //fromCharCode
 
-//if deosnt find the substring
-
-let str1 = "hi edwin";
-let position1 = str1.indexOf("kevin");
-console.log(position1); //prints value -1
-
-let str2 = "hi edwin";
-let position2 = str2.indexOf("edwin", 1);
-console.log(position2); //prints value -1
+let num2 = String.fromCharCode(66);
+console.log(num2);
