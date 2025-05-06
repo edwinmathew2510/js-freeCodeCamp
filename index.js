8072f371ce1d7a8a679df704c9df49d85f905145
Edwin Mathew <edwintm2510@gmail.com>
2025-05-06 13:26:51 +0530

    toUpperCase and toLowerCase method

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-//slice method to extract a substring
+//toUpperCase and toLowerCase method
 
-let name = "edwin mathew";
-let slice = name.slice(0, 5);
-console.log(slice);
+let name = "Edwin Mathew";
+let up = name.toUpperCase();
+console.log(up);
 
-let name1 = "edwin mathew";
-let slice1 = name1.slice(6); //extract methods after 6th index
-console.log(slice1);
-
-let name2 = "edwin mathew hi";
-let slice2 = name2.slice(-2); //extracts methods from back
-console.log(slice2);
+let lc = name.toLowerCase();
+console.log(lc);
