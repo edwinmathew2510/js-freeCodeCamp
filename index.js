f25db9f9c82d027c5d8b4787724db4fb5a8ab5de
Edwin Mathew <edwintm2510@gmail.com>
2025-05-19 22:20:33 +0530

    opretor precdence

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
-// calculations with Number  and other datatypes
+//opretor precdence
 
-value1 = 5 + "10";
-console.log(value1); //made both value string
-console.log(typeof value1);
+var result = 2 + 4 * 5 - 2;
+console.log(result); //* has more precdence value than +,-
 
-value2 = 5 - "10";
-console.log(value2);
-console.log(typeof value2); //made both into number
+//By using parentheses to force certain parts of the expression to be evaluated first.
+var result = 2 + 2 / 8 - 1;
+console.log(result); /// has more precendence value than +,-
 
-value3 = 5 * "10";
-console.log(value3);
-console.log(typeof value3); //made both into number
+var result = 10 - 2 + 5;
+console.log(result); //assoactivity left to right
 
-value4 = 5 / "10";
-console.log(value4);
-console.log(typeof value4); //made both into number
+var result = (b = 10);
+console.log(b, result); //assignment oprator has assotivity right to left
 
-value5 = 5 - "abc";
-console.log(value5);
-console.log(typeof value5); //made both into nan ,Applied to sub,div,mul
-
-// operations with boolean
-
-valuet = 5 + true;
-console.log(valuet);
-console.log(typeof valuet); //made both into number
-
-valuef = 5 + false;
-console.log(valuef);
-console.log(typeof valuef); //made both into number
-
-valuefs = "abc " + true;
-console.log(valuefs);
-console.log(typeof valuefs); //made both into string
-
-valueb = "5" / true;
-console.log(valueb);
-console.log(typeof valueb); //made both into number
+var result = 2 ** (3 ** 2); //exponet opeartor will assotive from right to left
+console.log(result);
