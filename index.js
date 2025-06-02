27d99b3af3a68d4fef4fa1f5a2aa57953384e2c4
Edwin Mathew <edwintm2510@gmail.com>
2025-06-02 09:16:19 +0530

    Masked email Funtion

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-// math object in js
+function maskedEmail(email) {
+  const atIndex = email.indexOf("@");
+  const userName = email.slice(0, atIndex);
+  const domain = email.slice(atIndex);
 
-const mathRandom = Math.random();
-console.log(mathRandom); //random no btw 0 and 1
+  const firstChar = userName[0];
+  const lastChar = userName[userName.length - 1];
 
-const min = Math.min(1, 4, 56, 74);
-console.log(min); //smallest no
+  const masked = firstChar + "*".repeat(userName.length - 2) + lastChar;
+  return masked + domain;
+}
 
-const max = Math.max(1, 2, 56, 89754, 345264);
-console.log(max); //largest no
-
-console.log(Math.ceil(2.3)); //round the value to next whole no
-
-console.log(Math.floor(3.4)); //round the no to pervious whole no
-
-console.log(Math.round(5.5)); //round the value more accurately
-
-const RandomNum = Math.floor(Math.random() * 10) + 1;
-console.log(RandomNum); //will choose a random no btw 1 to 10
-
-console.log(Math.trunc(2.3)); //remove the float
-
-console.log(Math.sqrt(81)); //square root
-
-console.log(Math.cbrt(8)); //cube root
-
-console.log(Math.abs(-5)); //absolute value
-
-console.log(Math.pow(2, 3)); //raises to the power of number
+function maskedPassword(pass) {
+  const passLength = pass.length;
+  const maskedPass = "*".repeat(passLength);
+  return maskedPass;
+}
+console.log(maskedEmail("edwintmw2510@gmail.com"));
+console.log(maskedPassword("edwin123"));
