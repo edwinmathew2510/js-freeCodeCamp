768306ae5dff59475bf15a12337acb80437ccb0e
Edwin Mathew <edwintm2510@gmail.com>
2025-06-02 09:56:58 +0530

    function isLeapYear

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-function maskedEmail(email) {
-  const atIndex = email.indexOf("@");
-  const userName = email.slice(0, atIndex);
-  const domain = email.slice(atIndex);
-
-  const firstChar = userName[0];
-  const lastChar = userName[userName.length - 1];
-
-  const masked = firstChar + "*".repeat(userName.length - 2) + lastChar;
-  return masked + domain;
+function isLeapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return `${year} its an leap year`;
+  } else {
+    return `${year} its not an leap year`;
+  }
 }
-
-function maskedPassword(pass) {
-  const passLength = pass.length;
-  const maskedPass = "*".repeat(passLength);
-  return maskedPass;
-}
-console.log(maskedEmail("edwintmw2510@gmail.com"));
-console.log(maskedPassword("edwin123"));
+const year = 2025;
+const result = isLeapYear(year);
+console.log(result);
