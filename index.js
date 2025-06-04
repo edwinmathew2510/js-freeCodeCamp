f46d35f97a1bef4b881d7c6764daed4fd8470632
Edwin Mathew <edwintm2510@gmail.com>
2025-06-04 20:35:07 +0530

    array function ex

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
-//split reverse and join methods
-
-let str = "hello";
-let arr = str.split("");
-console.log(arr);
-
-let revArr = arr.reverse();
-console.log(revArr);
-
-let revString = revArr.join("");
-console.log(revString);
-
-let str1 = "coding";
-let reverse = str1.split("").reverse().join("");
-console.log(reverse);
+let lunches = [];
+function addLunchToEnd(array, string) {
+  array.push(string);
+  console.log(string + " added to the end of the lunch menu.");
+  return array;
+}
+function addLunchToStart(array, string) {
+  array.unshift(string);
+  console.log(`${string} added to the start of the lunch menu.`);
+  return array;
+}
+function removeLastLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    let str = array.pop();
+    console.log(`${str}removed from the start of the lunch menu."`);
+  }
+  return array;
+}
+function removeFirstLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches to remove.");
+  } else {
+    let removed = array.shift();
+    console.log(removed + "removed from the start of the lunch menu.");
+    return array;
+  }
+}
+function getRandomLunch(array) {
+  if (array.length === 0) {
+    console.log("No lunches available.");
+  } else {
+    let randomIndex = math.floor(math.random() * array.length);
+    console.log(`Randomly selected lunch:${array[randomIndex]}`);
+    return array;
+  }
+}
+function showLunchMenu(array) {
+  if (array.length === 0) {
+    console.log("The menu is empty.");
+  } else {
+    console.log("Menu items:" + array.join(","));
+    return array;
+  }
+}
