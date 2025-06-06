f84f5c0f020894e1fcf4d10bc186a4c35fc97dfa
Edwin Mathew <edwintm2510@gmail.com>
2025-06-07 01:13:36 +0530

    object - array accesing

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
-//remove obj
+//nested object and accesing
+const person = {
+  name: "edwin",
+  age: 18,
+  contact: {
+    email: "edwintm@gmail.com",
+    phone: {
+      laneno: 1234567890,
+      mobno: 987654321,
+    },
+  },
+};
 
-const person = { name: "edwin", age: 18, city: "newyork" };
+console.log(person.contact.email);
+console.log(person["contact"]["phone"]["mobno"]); //accesing nested object
 
-// delete person.name;
+const person1 = {
+  name: "edwin",
+  age: 18,
+  address: [
+    { country: "india", state: "kerala", city: "ekm" },
+    { country: "america", state: "new jersy", city: "newyork" },
+  ],
+};
+console.log(person1.address[0].city);
+console.log(person1.address[1].state); // acccesing array-object values
 
-// console.log(person);
+//exersie
+let person3 = {
+  contact: {
+    phone: {
+      home: "123-456-7890",
+      work: "098-765-4321",
+    },
+  },
+};
+console.log(person3.contact.phone.work); //accesing work
+console.log(person3["contact"]["phone"]["work"]);
 
-const { name, ...remaining } = person;
+let data = {
+  results: [
+    { name: "Alice", score: 95 },
+    { name: "Bob", score: 80 },
+    { name: "Charlie", score: 90 },
+  ],
+};
 
-console.log(remaining);
-
-//how to check an  obj has a property
-
-const person1 = { name: "edwin", age: 18 };
-
-console.log(person1.hasOwnProperty("name")); //method 1
-console.log(person1.hasOwnProperty("city"));
-
-console.log("name" in person1); //method 2
-console.log("city" in person1);
-
-console.log(person.name !== undefined); //method 3
-console.log(person1.city !== undefined);
+console.log(data.results[1].name);
