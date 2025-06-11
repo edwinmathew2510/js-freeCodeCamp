42f5345966c3b520692855da4f646bd31f710a3a
Edwin Mathew <edwintm2510@gmail.com>
2025-06-11 15:07:39 +0530

    for in loop

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-const numbers = [0, 1, 2, 3, 4, 5];
-for (let num in numbers) {
-  console.log(num);
+//for in loop
+
+const fruits = {
+  name: "apple",
+  color: "red",
+  price: 25,
+};
+
+for (prop in fruits) {
+  console.log(fruits[prop]);
+}
+/////////////////////////////////////////////////////
+
+function isObject(value) {
+  return typeof value === "object" && !Array.isArray(value) && value !== null;
 }
 
-const str = "freecodecamp";
-for (let char in str) {
-  console.log(char);
-}
+const person = {
+  name: "edwin",
+  age: 21,
+  address: {
+    street: "cherai",
+    city: "ekm",
+  },
+  hobbies: ["football", "gaming"],
+};
 
-//for off loop
-const people = [
-  { name: "edwin", age: 21 },
-  { name: "kevin", age: 13 },
-  { name: "swapna", age: 40 },
-];
-for (const person of people) {
-  console.log(`${person.name} has ${person.age} years old`);
-}
-
-const colors = ["red", "green", "blue"];
-
-for (const color of colors) {
-  console.log(color);
+for (const prop in person) {
+  if (isObject(person[prop])) {
+    for (const nested in person[prop]) {
+      console.log(person[prop][nested]);
+    }
+  } else {
+    console.log(person[prop]);
+  }
 }
