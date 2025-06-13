374da4f36788e97b804d9e3042f1f4a9389ba5a9
Edwin Mathew <edwintm2510@gmail.com>
2025-06-13 11:20:32 +0530

    Build a Sentence Analyzer

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
-// // ///break statement
-
-for (let i = 1; i < 10; i++) {
-  if (i == 5) {
-    break;
-  }
-  console.log(i);
-}
-
-// //continue statement
-
-for (let i = 0; i < 10; i++) {
-  if (i === 5) {
-    continue;
-  }
-  console.log(i);
-}
-
-//label
-
-outerloop: for (let i = 0; i < 3; i++) {
-  innerloop: for (let j = 0; j < 3; j++) {
-    if (i == 1 && j == 1) {
-      break outerloop;
+//VowelCount
+function getVowelCount(sentence) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let lowerCaseSentence = sentence.toLowerCase();
+  let count = 0;
+  for (char of lowerCaseSentence) {
+    if (vowels.includes(char)) {
+      count++;
     }
-    console.log(`i:${i} , j:${j}`);
   }
+  return count;
 }
+console.log(getVowelCount("Apples are tasty fruits"));
+
+//ConsonantCount
+function getConsonantCount(sentence) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  for (const char of sentence.toLowerCase()) {
+    if (char >= "a" && char <= "z" && !vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(getConsonantCount("Hello,World!"));
+
+//PunctuationCount
+
+function getPunctuationCount(sentence) {
+  let clearSentence = sentence.replaceAll(" ", "");
+  count = 0;
+  for (let char of clearSentence.toLowerCase()) {
+    if (!(char >= "a" && char <= "z")) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+console.log(`Punctuation Count: ${punctuationCount}`);
+
+//WordCount
+function getWordCount(sentence) {
+  let words = sentence
+    .trim()
+    .split(" ")
+    .filter((word) => word !== "");
+  return words.length;
+}
+const wordCount = getWordCount("I love freeCodeCamp");
+console.log(`Word Count: ${wordCount}`);
