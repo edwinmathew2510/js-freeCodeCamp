51def5d462ec95becd3c17484e31c175a7e59547
Edwin Mathew <edwintm2510@gmail.com>
2025-06-13 19:09:54 +0530

    string & number methods

--- index.js
+++ index.js
@@ -1,44 +1,23 @@
-//factorial
-let num = 10;
-function factorialCalculator(num) {
-  let result = 1;
-  for (let i = result; i <= num; i++) {
-    result = result * i;
-  }
-  const resultMsg = `Factorial of ${num} is ${result}`;
-  return resultMsg;
-}
+//string constructor function
 
-console.log(factorialCalculator(num));
+const stringObject = new String("hello ,world!");
+console.log(stringObject);
 
-//Implement the Mutations Algorithm
+//toString methods()
 
-function mutation(arr) {
-  let first = arr[0].toLowerCase();
-  let second = arr[1].toLowerCase();
+const num = 10;
+console.log(num.toString());
 
-  for (let char of second) {
-    if (!first.includes(char)) {
-      return false;
-    }
-  }
-  return true;
-}
-console.log(mutation(["hello", "hey"]));
+console.log(num.toString(2)); // binary of 10
 
-console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+const arr = [1, 2, 3]; //array
 
-//exersises
-for (let i = 1; i < 6; i++) {
-  if (i === 4) break;
-  console.log(i);
-}
-const shoppingList = { tomatoes: 4, apples: 10 };
-for (const item in shoppingList) {
-  console.log(item);
-}
+console.log(arr.toString());
 
-const fruits = ["Mango", "Pineapple", "Oranges"];
-for (const fruit of fruits) {
-  console.log(fruit);
-}
+// Number constactor
+
+const myNum = new Number("32");
+console.log(myNum);
+
+const mynum = Number("32");
+console.log(mynum);
