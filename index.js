17610e5ae96d927f07a0b01791d12171b7aedb52
Edwin Mathew <edwintm2510@gmail.com>
2025-06-16 12:04:56 +0530

    js chtgpt exersise

--- index.js
+++ index.js
@@ -1,67 +1,55 @@
-//Closures
-
-function outer() {
-  let message = "hello";
-
-  function inner() {
-    console.log(message); //clousers
-  }
-  inner();
-}
-outer();
-
-message = "boom"; //private variable
-
-///////////////////////////////
-//count down
-function counter() {
+///// js exersise
+function countVowels(str) {
   let count = 0;
-  function increment() {
-    count++;
-    console.log("value increment :" + count);
+  let vowels = ["a", "e", "i", "o", "u"];
+  let strLowercase = str.toLowerCase();
+  for (let char of strLowercase) {
+    if (vowels.includes(char)) {
+      count++;
+    }
   }
-  return { increment };
+  return count;
 }
 
-let coun = counter();
-coun.increment();
-coun.increment();
-coun.increment();
+console.log(countVowels("Hello World"));
+/////////////////////////////
 
+function checkEvenOrOdd(num) {
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+console.log(checkEvenOrOdd(7));
+console.log(checkEvenOrOdd(10));
 ///////////////////////
-// point game
 
-function createGame() {
-  let score = 0;
-
-  function increaseScore(points) {
-    score += points;
-    console.log("scores is : " + points + " pts");
-  }
-  function decreaseScore(points) {
-    score -= points;
-    console.log("scores is : " + points + " pts");
-  }
-  function getScore() {
-    return score;
-  }
-  return { increaseScore, decreaseScore, getScore };
+function findMax(array) {
+  maxNum = Math.max(...array);
+  return maxNum;
 }
+console.log(findMax([3, 7, 2, 9, 5]));
 
-let game = createGame();
-game.increaseScore(5);
-game.increaseScore(5);
-game.decreaseScore(5);
-console.log("total score is : " + game.getScore());
+///////
 
-//////////////////////
-//multyply
-
-function createGreeter(greeting) {
-  return function (name) {
-    console.log(greeting + ", " + name);
-  };
+function reverseString(str) {
+  let newStr = str.split("").reverse().join("");
+  return newStr;
 }
-
-let sayHello = createGreeter("Hello");
-sayHello("Alice");
+console.log(reverseString("hello"));
+//////////
+function getRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  if (randomNumber % 3 == 0) {
+    result = "Fizz";
+  }
+  if (randomNumber % 5 == 0) {
+    result = "buzz";
+  }
+  if (randomNumber % 3 == 0 && randomNumber % 5 == 0) {
+    result = "Fizz Buzz";
+  }
+  return result;
+}
+console.log(getRandomNumber());
