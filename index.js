64904101162bce2a76c4ef0b10f454da6afdba97
Edwin Mathew <edwintm2510@gmail.com>
2025-06-17 14:22:18 +0530

    js modules

--- index.js
+++ index.js
@@ -1,53 +1,10 @@
-//student msg exersise
-function getAverage(array) {
-  let count = 0;
-  for (let i = 0; i < array.length; i++) {
-    count = count + array[i];
-  }
-  let avgMark = count / array.length;
-  return avgMark;
-}
-// console.log(getAverage([10, 20, 30]));
+// math js
 
-function getGrade(score) {
-  let grade = "";
-  if (score == 100) {
-    grade = "A+";
-  } else if (score >= 90 && score <= 99) {
-    grade = "A";
-  } else if (score >= 80 && score <= 89) {
-    grade = "B";
-  } else if (score >= 70 && score <= 79) {
-    grade = "C";
-  } else if (score >= 60 && score <= 69) {
-    grade = "D";
-  } else if (score >= 0 && score <= 59) {
-    grade = "F";
-  }
-  return grade;
+export function add(a, b) {
+  return a + b;
 }
-// console.log(getGrade(95));
-
-function hasPassingGrade(score) {
-  let value;
-  let grade = getGrade(score);
-  if (grade == "F") {
-    value = false;
-  } else {
-    value = true;
-  }
-  return value;
+export function sub(a, b) {
+  return a - b;
 }
-
-function studentMsg(array, score) {
-  let average = getAverage(array);
-  let grade = getGrade(score);
-  let isResult = hasPassingGrade(score);
-
-  if (isResult == true) {
-    return `Class average: ${average}. Your grade: ${grade}. You passed the course.`;
-  } else {
-    return `Class average: ${average}. Your grade: ${grade}. You failed the course.`;
-  }
-}
-console.log(studentMsg([56, 23, 89, 42, 75, 11, 68, 34, 91, 19], 100));
+let pi = 3.13;
+export { pi };
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,28 +1,5 @@
-function getAverage(array) {
-  let count = 0;
-  for (let i = 0; i < array.length; i++) {
-    count = count + array[i];
-  }
-  let avgMark = count / array.length;
-  return avgMark;
-}
-console.log(getAverage([10, 20, 30]));
+import { add, sub, pi } from "./index.js";
 
-function getGrade(score) {
-  let grade = "";
-  if (score == 100) {
-    grade = "A+";
-  } else if (score >= 90 && score <= 99) {
-    grade = "A";
-  } else if (score >= 80 && score <= 89) {
-    grade = "B";
-  } else if (score >= 70 && score <= 79) {
-    grade = "C";
-  } else if (score >= 60 && score <= 69) {
-    grade = "D";
-  } else if (score >= 0 && score <= 59) {
-    grade = "F";
-  }
-  return grade;
-}
-console.log(getGrade(65));
+console.log(add(2, 1));
+console.log(sub(2, 1));
+console.log(pi);
