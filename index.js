959d00af832963d09883502ea41ce02294c0d901
Edwin Mathew <edwintm2510@gmail.com>
2025-06-19 17:39:51 +0530

    high order methods

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-// for forEach method
-let numbers = [1, 2, 3, 4, 5];
+const number = [1, 2, 3, 4, 5];
 
-// numbers.forEach(function (number) {
-//   console.log(number * 2);
-// });
+//map method
+let double = number.map((num) => num * 2);
+console.log(number);
+console.log(double);
 
-// numbers.forEach((number) => console.log(number * 2));
+// filter method
+let filtered = number.filter((num) => num % 2 == 0);
+console.log(filtered);
+console.log(number); //desont affect og array
 
-numbers.forEach(function (number, index, array) {
-  console.log(number, index, array);
-});
+//reduce methods
 
-let words = ["hi", "hello", "hey"];
-words.forEach((word, index, array) => console.log(word, index));
+let reduce = number.reduce(
+  (acummlator, currentValue) => acummlator + currentValue
+);
+console.log(reduce);
+// method chaning
+
+let transcations = [
+  { amount: 100, type: "credit" },
+  { amount: 34, type: "cash" },
+  { amount: 80, type: "credit" },
+  { amount: 60, type: "cash" },
+  { amount: 320, type: "credit" },
+];
+
+let oprations = transcations
+  .filter((transcation) => transcation.type == "credit")
+  .map((transcation) => transcation.amount * 1.1)
+  .reduce((sum, amount) => sum + amount, 0);
+
+console.log(oprations);
+
+//// sorted methods
+
+let toSorted = [45, 576, 34532, 87568, 567, 234, 87];
+
+toSorted.sort((a, b) => a - b);
+console.log(toSorted);
+
+//every method
+
+let every = number.every((num) => num % 2 == 0);
+console.log(every);
+
+//some method
+
+let some = number.some((num) => num % 2 == 0);
+console.log(some);
