a92e76d3f19afd77bd113c715daee5166e8607b4
Edwin Mathew <edwintm2510@gmail.com>
2025-06-22 20:33:00 +0530

    dom 4 , event object , add evenr listeer

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-// dom3, interfaces;
+// event object
+let button = document.getElementById("myButton");
+button.addEventListener("click", function (event) {
+  console.log("clicked");
 
-//navigator interface
-console.log(navigator.userAgent);
-console.log(navigator.language); //user preferred language
+  console.log(event.type); // type property
 
-//window interface
+  console.log(event.target);
 
-console.log(window.innerWidth); //show thr inner width
-console.log(window.location); // local url location
+  console.log(event.preventDefault);
+});
+document
+  .getElementById("inputField")
+  .addEventListener("keydown", function (event) {
+    console.log(event.key);
+    console.log(event.keyCode);
+  });
+//mouse area
+document
+  .getElementById("mouseArea")
+  .addEventListener("mousemove", function (event) {
+    console.log("mouse y:" + event.clientY);
+    console.log("mouse x:" + event.clientX);
+  });
 
-//document interface
+///form
+document.getElementById("myForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log("form submission prevented");
+});
+//mouse enter &left
 
-console.log(document.children);
+let hoverBox = document.getElementById("hover-box");
 
-///////////////////////////////
+hoverBox.addEventListener("mouseenter", function (event) {
+  console.log("mouse Entered:", event.target);
+});
 
-//setAr-trributes
+hoverBox.addEventListener("mouseleave", function (event) {
+  console.log("mouse left:", event.target);
+});
 
-const para = document.getElementById("container1");
-para.setAttribute("class", "class-1");
-// class attribute is added
+//btn
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function (event) {
+  alert("you clicked");
+});
 
-para.setAttribute("class", "example");
-//class attribute is updated
+//input
+let input = document.getElementById("input");
+input.addEventListener("input", function (event) {
+  console.log(input.value);
+});
