523a4ca13142e0c58afb6644e25ae9eac103aae9
Edwin Mathew <edwintm2510@gmail.com>
2025-06-23 17:30:33 +0530

    dom 5,remove event lister and exersie

--- index.js
+++ index.js
@@ -1,53 +1,38 @@
-// event object
-let button = document.getElementById("myButton");
-button.addEventListener("click", function (event) {
-  console.log("clicked");
-
-  console.log(event.type); // type property
-
-  console.log(event.target);
-
-  console.log(event.preventDefault);
-});
-document
-  .getElementById("inputField")
-  .addEventListener("keydown", function (event) {
-    console.log(event.key);
-    console.log(event.keyCode);
-  });
-//mouse area
-document
-  .getElementById("mouseArea")
-  .addEventListener("mousemove", function (event) {
-    console.log("mouse y:" + event.clientY);
-    console.log("mouse x:" + event.clientX);
-  });
-
-///form
-document.getElementById("myForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log("form submission prevented");
-});
-//mouse enter &left
-
-let hoverBox = document.getElementById("hover-box");
-
-hoverBox.addEventListener("mouseenter", function (event) {
-  console.log("mouse Entered:", event.target);
-});
-
-hoverBox.addEventListener("mouseleave", function (event) {
-  console.log("mouse left:", event.target);
-});
-
+//dom 5
 //btn
 let btn = document.getElementById("btn");
-btn.addEventListener("click", function (event) {
-  alert("you clicked");
-});
+function handelClick() {
+  alert("hello");
+}
+btn.addEventListener("click", handelClick);
+
+setTimeout(() => {
+  btn.removeEventListener("btn", handelClick);
+  console.log("event listener canceled");
+}, 1000);
 
 //input
 let input = document.getElementById("input");
-input.addEventListener("input", function (event) {
+function handelInput() {
   console.log(input.value);
+}
+input.addEventListener("input", handelInput);
+
+// input.removeEventListener("input", handelInput);
+
+/////////////// exercise
+let bodyEl = document.querySelector("body");
+let para = document.getElementById("para");
+let btnn = document.getElementById("btnn");
+
+let isbgclgrey = true;
+function togglefun() {
+  bodyEl.style.backgroundColor = isbgclgrey ? "blue" : "grey";
+  isbgclgrey = !isbgclgrey;
+}
+
+btnn.addEventListener("click", togglefun);
+
+para.addEventListener("mouseover", () => {
+  btnn.removeEventListener("click", togglefun);
 });
