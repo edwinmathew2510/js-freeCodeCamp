eaaa0ad82400bc1349e93cdfa8b47ed583b789c9
Edwin Mathew <edwintm2510@gmail.com>
2025-07-14 21:37:41 +0530

    debugging 2 // try catch error handling

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-// debugging;
+// debugging 2
+// try catch error handling
 
-function validate(input) {
-  if (typeof input !== "number") {
-    throw new TypeError(
-      "Expected input should be number ,but received input : " + typeof input
-    );
+function greet(name) {
+  if (typeof name !== "string") {
+    throw new TypeError("input should be a string");
   }
-  return input * 2;
+  return "hi " + name;
 }
-console.log(validate(1));
-
-///////////////////////////////////
-
-function divide(n, d) {
-  if (d === 0) {
-    throw new TypeError("Can divide with Zero ");
-  }
-  return n / d;
+try {
+  let result = greet(2);
+  console.log("result :" + result);
+} catch (error) {
+  console.error("Error:", error.message);
+} finally {
+  console.log("Execution completed.");
 }
-console.log(divide(5, 0));
+
+// let firstNumber = 23;
+// let secondNumber = 4;
+// let sum = firstNumber + secondNumber;
+// console.log(sum);
+
+// const obj = [
+//   { id: 1, name: "edwin", sex: "male" },
+//   { id: 2, name: "kevin", sex: "male" },
+//   { id: 3, name: "swapna", sex: "female" },
+// ];
+
+// console.table(obj);
+// console.dir(obj);
+// document.getElementById;
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,14 @@
+let firstNumber = 23;
+// let secondNumber = 4;
+// let sum = firstNumber + secondNumber;
+// console.log(sum);
+
+// const obj = [
+//   { id: 1, name: "edwin", sex: "male" },
+//   { id: 2, name: "kevin", sex: "male" },
+//   { id: 3, name: "swapna", sex: "female" },
+// ];
+
+// console.table(obj);
+// console.dir(obj);
+// document.getElementById;
